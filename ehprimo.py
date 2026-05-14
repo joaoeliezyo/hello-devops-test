ab0dabd37ca1fd24047327c1768790a0ec4ba9ff
João Eliezyo <joaoeliezyo@gmail.com>
2026-05-14 20:32:56 -0300

    inicial

---
+++ ehprimo.py
@@ -0,0 +1,20 @@
+def eh_primo(num):
+    "Verifica se um número é primo."
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def enesimo_primo(n):
+    "Encontra o n-ésimo número primo."
+    contador = 0
+    numero_atual = 1
+    
+    while contador < n:
+        numero_atual += 1
+        if eh_primo(numero_atual):
+            contador += 1
+            
+    return numero_atual
---
+++ teste.py
@@ -0,0 +1,4 @@
+from calculadora import soma
+assert enesimo_primo(10) == 29
+
+print("Teste executado com sucesso")
